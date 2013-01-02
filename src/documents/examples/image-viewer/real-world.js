9adc43386f17d7462653f64d97a5be2da38da542
ROBERT_FRAMPTON <ROBERT.FRAMPTON@LIFERAY.COM>
2013-01-02 15:37:06 -0800

    Alloyui.com - Example - Image Viewer (real world example)

--- src/documents/examples/image-viewer/real-world.js
+++ src/documents/examples/image-viewer/real-world.js
@@ -1,1 +1,31 @@
-// Hello world
+AUI().ready('aui-image-viewer', 'aui-media-viewer-plugin', function(A) {
+
+  var mediaViewer = new A.ImageViewer({
+    links: '#gallery a',
+    plugins: [
+      {
+        // Daily Motion
+        cfg: {
+          'providers.dailymotion': {
+            container: '<iframe frameborder="0" width="{width}" height="{height}" src="http://www.dailymotion.com/embed/video/{media}"></iframe>',
+            matcher: new RegExp(
+              A.Lang.sub(
+                A.MediaViewerPlugin.REGEX_DOMAIN,
+                {
+                  domain: 'dailymotion.com'
+                }
+              ),
+              'i'
+            ),
+            options: A.MediaViewerPlugin.DEFAULT_OPTIONS,
+            mediaRegex: /\/video\/([^&#_]*)/i
+          }
+        },
+        fn: A.MediaViewerPlugin
+      }
+    ],
+    preloadNeighborImages: true
+  });
+
+  mediaViewer.render();
+});
